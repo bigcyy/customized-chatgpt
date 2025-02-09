31ec69186d18580066d84e99dc381bbad2d13a4d
Chen Yang <1597081640@qq.com>
2025-02-09 21:38:13 +0800

    enhancement#8: 更新 SpringBoot 版本

--- src/main/java/com/cyy/chat/controller/ChatController.java
+++ src/main/java/com/cyy/chat/controller/ChatController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
